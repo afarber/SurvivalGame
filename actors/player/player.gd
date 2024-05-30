495d1c1fc7c2244fa8b930e4c926dbebea8ab453
Alexander Farber <farber72@outlook.de>
2024-05-30 19:07:56 +0200

    Implement player movement, jump and sprint

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -11,15 +11,43 @@ extends CharacterBody3D
 
 @onready var head: Node3D = $Head
 
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _physics_process(delta: float) -> void:
 	move()
 
 func move() -> void:
+	var is_sprinting := false
+	
+	if is_on_floor():
+		is_sprinting = Input.is_action_pressed("sprint")
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = jump_velocity
+	else:
+		velocity.y -= gravity
+		
+	var speed := sprint_speed if is_sprinting else normal_speed
+
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := transform.basis * Vector3(input_dir.x, 0,  input_dir.y)
-	velocity.x = direction.x * normal_speed
-	velocity.y = direction.y * normal_speed
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	move_and_slide()
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		look_around(event.relative)
+
+func look_around(relative: Vector2) -> void:
+	rotate_y(-relative.x * mouse_sensitivity)
+	head.rotate_x(-relative.y * mouse_sensitivity)
+	# down -90, up 90
+	head.rotation_degrees.x = clampf(head.rotation_degrees.x, -90, 90)
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if (event.is_action_pressed("ui_cancel")):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
