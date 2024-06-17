9fe7a450d0b51e3993819b85d0cb984ca6bc873e
Alexander Farber <farber72@outlook.de>
2024-06-17 18:01:25 +0200

    Add equippable_item script

---
+++ items/equippables/equippable_item.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+class_name EquippableItem
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func try_to_use() -> void:
+	if animation_player.is_playing():
+		return
+	animation_player.play("use_item")
