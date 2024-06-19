10cac3524760def6680fda7664f2c7abe61c72f8
Alexander Farber <farber72@outlook.de>
2024-06-19 22:25:48 +0200

    Send signals when consumed

--- items/equippables/equippable_consumable_template.gd
+++ items/equippables/equippable_consumable_template.gd
@@ -2,3 +2,11 @@ extends EquippableItem
 class_name EquippableConsumable
 
 var consumable_item_resource: ConsumableItemResource
+
+func consume() -> void:
+	EventSystem.PLY_change_health.emit(consumable_item_resource.health_change)
+	EventSystem.PLY_energy_health.emit(consumable_item_resource.energy_change)
+	EventSystem.EQU_delete_equipped_item.emit()
+
+func destroy_self() -> void:
+	EventSystem.EQU_unequip_item.emit()
