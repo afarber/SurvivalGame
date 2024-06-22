e7c2b5b83db429a8770fb38de27b1b6d6c4bbe69
Alexander Farber <farber72@outlook.de>
2024-06-22 18:10:06 +0200

    Start adding animal template

---
+++ actors/animals/animal.gd
@@ -0,0 +1,53 @@
+extends CharacterBody3D
+class_name Animal
+
+enum States {
+	Idle,
+	Wander,
+	Dead
+}
+
+var state := States.Idle
+
+@onready var idle_timer: Timer = %IdleTimer
+@onready var wander_timer: Timer = %WanderTimer
+@onready var disappear_after_death_timer: Timer = %DisappearAfterDeathTimer
+
+@onready var main_collision_shape: CollisionShape3D = $CollisionShape3D
+@onready var meat_spawn_marker: Marker3D = $MeatSpawnMarker
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+
+@export var normal_speed := 0.6
+@export var max_health := 80.0
+@export var idle_animations: Array[String] = []
+@export var turn_speed_weight := 0.07
+@export var min_idle_time := 2.0
+@export var max_idle_time := 7.0
+@export var min_wander_time := 2.0
+@export var max_wander_time := 4.0
+
+var health := max_health
+
+func _ready() -> void:
+	animation_player.animation_finished.connect(animation_finished)
+
+func animation_finished(_animation_name: String) -> void:
+	if state == States.Idle:
+		animation_player.play(idle_animations.pick_random())
+
+func _physics_process(_delta: float) -> void:
+	if state == States.Wander:
+		wander_loop()
+
+func wander_loop() -> void:
+	look_forward()
+	move_and_slide()
+
+func look_forward() -> void:
+	# rotate the animal into same direction in which it is moving
+	rotation.y = lerp_angle(rotation.y, atan2(velocity.x, velocity.z) + PI, turn_speed_weight)
+
+func pick_wander_velocity() -> void:
+	# forward, then rotate by random value
+	var dir := Vector2(0, -1).rotated(randf() * PI * 2)
+	velocity = Vector3(dir.x, 0, dir.y) * normal_speed
