6334d38507e844c7509e515d13f4bc879d12a3a0
Alexander Farber <farber72@outlook.de>
2024-07-05 21:12:30 +0200

    Add valid and invalid materials with green and red albedo

--- items/equippables/equippable_constructable.gd
+++ items/equippables/equippable_constructable.gd
@@ -1,7 +1,16 @@
 extends EquippableItem
 class_name EquippableConstructable
 
+# preload() loads immediately when the game starts, load() when it is needed
+const VALID_MATERIAL:StandardMaterial3D = preload("res://resources/materials/constructable_valid_material.tres")
+const INVALID_MATERIAL:StandardMaterial3D = preload("res://resources/materials/constructable_invalid_material.tres")
+
 @onready var item_place_ray: RayCast3D = $ItemPlaceRay
 @onready var constructable_area: Area3D = $ConstructableArea
 @onready var collision_shape_3d: CollisionShape3D = $ConstructableArea/CollisionShape3D
 @onready var constructable_preview_mesh: MeshInstance3D = $ConstructableArea/ConstructablePreviewMesh
+
+var constructable_item_key: ItemConfig.Keys
+var obstacles: Array[Node3D] = []
+var place_valid := false
+
