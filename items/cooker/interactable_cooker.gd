cf303d78c69a440c7a0b88d1af075350156ae332
Alexander Farber <farber72@outlook.de>
2024-07-16 18:54:55 +0200

    Add CookingStates enum

--- items/cooker/interactable_cooker.gd
+++ items/cooker/interactable_cooker.gd
@@ -3,8 +3,26 @@ class_name InteractableCooker
 
 @onready var cooking_timer: Timer = $CookingTimer
 @onready var food_visuals_holder: Marker3D = $FoodVisualsHolder
-@onready var gpu_particles: GPUParticles3D = $GPUParticles3D
-@onready var omni_light: OmniLight3D = $OmniLight3D
+@onready var fire_particles: GPUParticles3D = $GPUParticles3D
+@onready var fire_light: OmniLight3D = $OmniLight3D
+
+var cooking_recipe: CookingRecipeResource
+
+enum CookingStates {
+	Inactive,
+	ReadyToCook,
+	Cooking,
+	Cooked
+}
+
+var state := CookingStates.Inactive
 
 func start_interaction() -> void:
-	EventSystem.BUL_create_bulletin.emit(BulletinConfig.Keys.CookingMenu)
+	EventSystem.BUL_create_bulletin.emit(
+		BulletinConfig.Keys.CookingMenu,
+		[
+			cooking_recipe,
+			(cooking_recipe.cooking_time - cooking_timer.time_left) if cooking_recipe else 0,
+			self
+		]
+		)
